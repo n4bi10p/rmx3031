72a79d65f09c03d517644edd5b7ffb3440cf911e
Nabil Shaikh <snabilshaikh186@gmail.com>
2023-09-02 11:51:44 +0530

    Update vendorsetup.sh

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -3,7 +3,7 @@ VT=vendor/realme/RMX3031/RMX3031-vendor.mk
 if ! [ -a $VT ]; then git clone https://github.com/n4bi10p/proprietary_vendor_realme_RMX3031 vendor/realme/RMX3031
 fi
 KT=kernel/realme/RMX3031/Makefile
-if ! [ -a $KT ]; then git clone --depth=1 https://github.com/n4bi10p/android_kernel_realme_mt6893 kernel/realme/mt6893
+if ! [ -a $KT ]; then git clone --depth=1 https://github.com/n4bi10p/kernel_realme_RMX3031 -b c12 kernel/realme/mt6893
 fi
 git clone https://github.com/n4bi10p/manshu_vendor_realme_RMX3031-firmware vendor/realme/RMX3031-firmware
 
